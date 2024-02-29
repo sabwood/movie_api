e643d058217d4c547983f2a1c978928aa4d502b4
sabwood <150414048+sabwood@users.noreply.github.com>
2024-02-28 16:39:15 -0800

    created Express routing
    created Express routing

--- index.js
+++ index.js
@@ -1,69 +1,142 @@
 const express = require('express'),
+bodyParser = require('body-parser'),
+uuid = require('uuid'),
 morgan = require('morgan');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use(morgan('common'));
+
+let users = [
+    {
+        id: 1,
+        username: 'exampleUsername',
+        name: 'exampleName',
+        favoriteMovies: ['movieOne']
+    }
+]
 
 let topMovies = [
     {
         title: 'movieOne',
-        genre: 'genreExample',
-        director: 'directorOne',
+        genre: {
+            genreName: 'genreNameExample',
+            genreDescription: 'descriptionExample'
+        },
+        director: {
+            directorName: 'directorNameExample',
+            birthYear: '####'
+        },
         description: 'descriptionOne'
     },
     {
         title: 'movieTwo',
-        genre: 'genreExample',
-        director: 'directorTwo',
+        genre: {
+            genreName: 'genreNameExample',
+            genreDescription: 'descriptionExample'
+        },
+        director: {
+            directorName: 'directorNameExample',
+            birthYear: '####'
+        },
         description: 'descriptionTwo'
     },
     {
         title: 'movieThree',
-        genre: 'genreExample',
-        director: 'directorThree',
+        genre: {
+            genreName: 'genreNameExample',
+            genreDescription: 'descriptionExample'
+        },
+        director: {
+            directorName: 'directorNameExample',
+            birthYear: '####'
+        },
         description: 'descriptionThree'
     },
     {
         title: 'movieFour',
-        genre: 'genreExample',
-        director: 'directorFour',
+        genre: {
+            genreName: 'genreNameExample',
+            genreDescription: 'descriptionExample'
+        },
+        director: {
+            directorName: 'directorNameExample',
+            birthYear: '####'
+        },
         description: 'descriptionFour'
     },
     {
         title: 'movieFive',
-        genre: 'genreExample',
-        director: 'directorFive',
+        genre: {
+            genreName: 'genreNameExample',
+            genreDescription: 'descriptionExample'
+        },
+        director: {
+            directorName: 'directorNameExample',
+            birthYear: '####'
+        },
         description: 'descriptionFive',
     },
     {
         title: 'movieSix',
-        genre: 'genreExample',
-        director: 'directorSix',
+        genre: {
+            genreName: 'genreNameExample',
+            genreDescription: 'descriptionExample'
+        },
+        director: {
+            directorName: 'directorNameExample',
+            birthYear: '####'
+        },
         description: 'descriptionSix'
     },
     {
         title: 'movieSeven',
-        genre: 'genreExample',
-        director: 'directorSeven',
+        genre: {
+            genreName: 'genreNameExample',
+            genreDescription: 'descriptionExample'
+        },
+        director: {
+            directorName: 'directorNameExample',
+            birthYear: '####'
+        },
         description: 'descriptionSeven'
     },
     {
         title: 'movieEight',
-        genre: 'genreExample',
-        director: 'directorEight',
+        genre: {
+            genreName: 'genreNameExample',
+            genreDescription: 'descriptionExample'
+        },
+        director: {
+            directorName: 'directorNameExample',
+            birthYear: '####'
+        },
         description: 'descriptionEight'
     },
     {
         title: 'movieNine',
-        genre: 'genreExample',
-        director: 'directorNine',
+        genre: {
+            genreName: 'genreNameExample',
+            genreDescription: 'descriptionExample'
+        },
+        director: {
+            directorName: 'directorNameExample',
+            birthYear: '####'
+        },
         description: 'descriptionNine'
     },
     {
         title: 'movieTen',
-        genre: 'genreExample',
-        director: 'directorTen',
+        genre: {
+            genreName: 'genreNameExample',
+            genreDescription: 'descriptionExample'
+        },
+        director: {
+            directorName: 'directorNameExample',
+            birthYear: '####'
+        },
         description: 'descriptionTen'
     }
 ];
@@ -77,6 +150,107 @@ app.get('/', (req, res) => {
 app.get('/movies', (req, res) => {
     res.json(topMovies);
 });
+
+app.get('/movies/:title', (req, res) => {
+    let title = req.params.title;
+    let movie = topMovies.find( movie => movie.title === title);
+
+    if (!movie) {
+        res.status(400).send('There is no such movie.');
+    } else {
+        res.status(200).json(movie)
+    }
+});
+
+app.get('/movies/genre/:genreName', (req, res) => {
+    let genreName = req.params.genreName;
+    let genre = topMovies.find( movie => movie.genre.genreName === genreName).genre;
+
+    if (!genre) {
+        res.status(400).send('There is no such genre.');
+    } else {
+        res.status(200).json(genre)
+    }
+});
+
+app.get('/movies/directors/:directorName', (req, res) => {
+    let directorName = req.params.directorName;
+    let director = topMovies.find( movie => movie.director.directorName === directorName ).director;
+
+    if (director) {
+        res.status(200).json(director);
+    } else {
+        res.status(400).send('There is no such director.')
+    }
+});
+
+app.post('/users', (req,res) => {
+    let newUser = req.body;
+
+    if (!newUser.username) {
+        const message = 'Missing username in request body.';
+        res.status(400).send(message);
+    } else {
+        newUser.id = uuid.v4();
+        users.push(newUser);
+        res.status(201).json(newUser);
+    }
+});
+
+app.put('/users/:id', (req,res) => {
+    let id = req.params.id;
+    let updatedUsername = req.body;
+
+    let user = users.find( user => user.id === id );
+
+    if (!user) {
+        res.status(400).send('There is no such user.')
+    } else {
+        user.username = updatedUsername.username;
+        res.status(200).json(user);
+    }
+});
+
+app.put('/users/:id/:movieTitle', (req, res) => {
+    let id = req.params.id;
+    let movieTitle = req.params.movieTitle;
+
+    let user = users.find( user => user.id === id );
+
+    if (!user) {
+        res.status(400).send('There is no such user.')
+    } else {
+        user.favoriteMovies.push(movieTitle);
+        res.status(200).send(movieTitle + 'has been added to ' + id + '\'s favorite\'s list.');
+    }
+});
+
+app.delete('/users/:id/:movieTitle', (req,res) => {
+    let id = req.params.id;
+    let movieTitle = req.params.movieTitle;
+
+    let user = users.find( user => user.id === id );
+
+    if (!user) {
+        res.status(400).send('There is no such user.')
+    } else {
+        user.favoriteMovies = user.favoriteMovies.filter((title) => title !== movieTitle);
+        res.status(200).send(movieTitle + 'has been removed from ' + id + '\'s favorite\'s list.');
+    }
+});
+
+app.delete('/users/:id', (req, res) => {
+    let id = req.params.id;
+
+    let user = users.find( user => user.id === id );
+
+    if (!user) {
+        res.status(400).send('There is no such user.')
+    } else {
+        users = users.filter( user => user.id != id);
+        res.status(200).send('User ' + id + ' has been deleted.');
+    }
+})
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
